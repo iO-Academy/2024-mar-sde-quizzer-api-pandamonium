b733a8cf3e6b676fac242eebae00f696574187ee
Kate <kate.wileman@hotmail.co.uk>
2024-06-10 11:55:54 +0100

    initial setup & quiz table seeded

---
+++ database/seeders/QuizSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class QuizSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('quizzes')->insert([
+                'name' => Str::random(10),
+                'description' => Str::random(500)
+            ]);
+        }
+
+    }
+}
