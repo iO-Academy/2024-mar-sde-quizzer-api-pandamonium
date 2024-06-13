29c7335534ce8e1baa51ba720a3c2a82c350e1c9
Rich <richowen87@icloud.com>
2024-06-13 12:29:37 +0100

    scores stuff added

--- app/Http/Controllers/ScoreAPIController.php
+++ app/Http/Controllers/ScoreAPIController.php
@@ -3,15 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Models\Score;
+use Exception;
 use Illuminate\Http\Request;
 
 class ScoreAPIController extends Controller
 {
     public function addScores(Request $request)
     {
+        $request->validate([
+            'quiz' => 'required|integer|exists:quizzes,id',
+            'answers' => 'required'
+        ]);
+
         $score = new Score();
         $score->quiz = $request->quiz;
         $score->answers = $request->answers;
-        $score->save();
+
+        try {
+            $result = $score->save();
+        } catch (Exception $e) {
+            $result = false;
+        }
+
+        if ($result === true) {
+            return response()->json(["message" => 'Score calculated'], 200);
+        } else {
+            return response()->json([
+                "message" => " "
+            ], 500);
+        }
     }
 }
